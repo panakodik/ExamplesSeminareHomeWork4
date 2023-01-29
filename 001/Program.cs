fcc7347702a334b68635498085d7c1cb1cefe15c
Aleksandr <pandom@yandex.ru>
2023-01-30 00:39:37 +0300

    001 0/1 array

---
+++ 001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами. вывести их на экран
+int[] array = new int[8]; //Задаем масиив
+int length = array.Length;
+/*
+int index = 0;
+while (index < length)
+{
+    array[index] = new Random().Next(0, 2);
+    index++;
+}                          // задаем массив
+*/
+for (int i = 0; i < length; i++) // Так мне нравится больше!
+{
+    array[i] = new Random().Next(0, 2);
+}
+Console.WriteLine("{0}", string.Join("; ", array)); //Вывод заданного массива
