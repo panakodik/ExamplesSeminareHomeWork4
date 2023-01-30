9138ad823ebcc958cb5438df1e5a72ca5412a5ad
Aleksandr <pandom@yandex.ru>
2023-01-31 01:31:56 +0300

    007 Разница макс и мин значений массива

---
+++ 007/Program.cs
@@ -0,0 +1,27 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.Clear();
+Console.WriteLine();
+int size = new Random().Next(1, 11);//определяем количество элементов массива
+Console.WriteLine($"Количетсов элементов массива: {size}");
+double[] AssignArray(int size)// метод заполнения элементов массива вещественными числами
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 10;
+    }
+    return array;
+}
+void DiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min(); //  вычисление разницы максимального и минимального значения элемента массива
+    Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    Console.WriteLine();
+}
+DiffMinMax(AssignArray(size));
+Console.WriteLine();
